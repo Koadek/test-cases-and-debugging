700220863766a6e5ab9fd2daa75acb9d3a6f3bb0
Cyril <kenny_mccormick2@hotmail.com>
2019-04-11 19:19:47 -0400

    'Enter'

--- problems/problem1.js
+++ problems/problem1.js
@@ -1,18 +1,21 @@
-let verifyEquals = require('./verify-equals.js'); // this line imports the verifyEquals function from the verify-equals.js file
+let verifyEquals = require("./verify-equals.js"); // this line imports the verifyEquals function from the verify-equals.js file
 
 // we need 5 test cases. I provided 1 input
-let inputs = ['max'];
+let inputs = ["max", "qwerf", "des zaffaires", "zgueg", ""];
 
-let outputs = ['m'];
+let outputs = ["m", "q", "d", "z", undefined];
 
 // Make this function return the first character of the string that is passed to it. If the string does not have a first character, return undefined
 function f(str) {
+  if (str.charAt(0)="") {
+    return undefined;
+  }
   return str.charAt(0);
 }
 
 //This function runs a test. You do not need to change any code under here
 function runTest(i) {
-  if (i >= inputs.length) throw new Error('You do not have enough test cases');
+  if (i >= inputs.length) throw new Error("You do not have enough test cases");
   let expected = outputs[i];
   let actual = f(inputs[i]);
   verifyEquals(expected, actual); // verifyEquals is imported above
@@ -23,4 +26,4 @@ runTest(1);
 runTest(2);
 runTest(3);
 runTest(4);
-console.log('All tests passed for ' + __filename);
+console.log("All tests passed for " + __filename);
