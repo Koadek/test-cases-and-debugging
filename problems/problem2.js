0c547fd7dbd496fe097005101e69c097a5eeeada
Cyril <kenny_mccormick2@hotmail.com>
2019-04-11 19:32:07 -0400

    'Enter'

--- problems/problem2.js
+++ problems/problem2.js
@@ -1,16 +1,21 @@
-let verifyEquals = require('./verify-equals.js');
+let verifyEquals = require("./verify-equals.js");
 
 // we need 5 test cases.
-let inputs = ['max', 'fghj', ''];
+let inputs = ["max", "fghj", "", "12344", "qwerf zgueg"];
 
-let outputs = ['x', 'j', undefined];
+let outputs = ["x", "j", undefined, "4", "g"];
 
 // Make this function return the last character of the string that is passed to it. If the string does not have a last character, return undefined
-function f(str) {}
+function f(str) {
+  if ((lastChar = myString.charAt(myString.length() - 1) === "")) {
+    return undefined;
+  }
+  return (lastChar = myString.charAt(myString.length() - 1));
+}
 
 //This function runs a test. You do not need to change any code under here
 function runTest(i) {
-  if (i >= inputs.length) throw new Error('You do not have enough test cases');
+  if (i >= inputs.length) throw new Error("You do not have enough test cases");
   let expected = outputs[i];
   let actual = f(inputs[i]);
   verifyEquals(expected, actual);
@@ -21,4 +26,4 @@ runTest(1);
 runTest(2);
 runTest(3);
 runTest(4);
-console.log('All tests passed for ' + __filename);
+console.log("All tests passed for " + __filename);
