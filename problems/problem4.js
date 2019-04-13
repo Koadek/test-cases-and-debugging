b70ee66c9f6ddaeff32551e18cf9afd93de0b9ed
Cyril <kenny_mccormick2@hotmail.com>
2019-04-12 20:45:38 -0400

    'Enter'

--- problems/problem4.js
+++ problems/problem4.js
@@ -1,24 +1,49 @@
-let verifyEquals = require('./verify-equals.js');
+let verifyEquals = require("./verify-equals.js");
 
 // we need 8 test cases. I've provided the first 2
-let inputs = [['hello', 4], ['', 2]];
+let inputs = [
+  ["hello", 4],
+  ["", 2],
+  ["abc", 0],
+  ["salut ça va?", 5],
+  ["12345", 3],
+  ["   talkfj", 2],
+  ["   talkfj", 7],
+  ["abc", 3]
+];
 
-let outputs = ['o', undefined];
+let outputs = [
+  "o",
+  undefined,
+  "a",
+  undefined,
+  undefined,
+  undefined,
+  "f",
+  undefined
+];
 
 /*
 Make this function return the letter at the specified position in the string. If no such letter exists, it should return undefined.
-
 For example:
 f(["hello", 1]); // e
 f(["", 4]); // undefined
 f(["abc", 0]); // a
 
 */
-function f(arr) {}
+function f(arr) {
+  function isLetter(c) {
+    return c.toLowerCase() != c.toUpperCase();
+  }
+  if (isLetter(arr[0].charAt(arr[1])) !== true) {
+    return undefined;
+  }
+  return arr[0].charAt(arr[1]);
+}
 
 //This function runs a test. You do not need to change any code under here
 function runTest(i) {
-  if (i >= inputs.length) throw new Error('You do not have enough test cases');
+  if (i >= inputs.length) throw new Error("You do not have enough test cases");
   let expected = outputs[i];
   let actual = f(inputs[i]);
   verifyEquals(expected, actual);
@@ -32,4 +57,4 @@ runTest(4);
 runTest(5);
 runTest(6);
 runTest(7);
-console.log('All tests passed for ' + __filename);
+console.log("All tests passed for " + __filename);
